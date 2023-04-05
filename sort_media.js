99ca75b8e4203f095449636b816fe68db38475d2
JDerbin05 <113055032+JDerbin05@users.noreply.github.com>
2023-04-05 12:47:27 -0400

    Add files via upload

---
+++ sort_media.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Select all unordered lists with class "sort-me"
+    const listsToSort = document.querySelectorAll("ul.sort-me");
+
+// Loop through each unordered list
+listsToSort.forEach((list) => {
+    // Select all list items within the current list
+    const listItems = Array.from(list.querySelectorAll("li:not(.nav-link)"));
+
+    // Sort the list items by text content
+    listItems.sort((a, b) => a.textContent.localeCompare(b.textContent));
+
+    // Append the sorted list items back to the original list
+    listItems.forEach((item) => list.appendChild(item));
+});
